38a9a4a4b69aad1a5a54a3b46717e1388540bde6
Hyunwook Jo <acerghjk@gmail.com>
2023-06-16 15:36:36 +0900

    Update get_pytorch_bin.py

--- get_pytorch_bin.py
+++ get_pytorch_bin.py
@@ -10,22 +10,24 @@ def get_pytorch_bin(args):
   if args.drive:
     config = T5Config.from_pretrained(f"{model_name}")
     model = T5Model(config)
-    load_flax_checkpoint_in_pytorch_model(model, f"/content/{model_name}/flax_model.msgpack")
+    load_flax_checkpoint_in_pytorch_model(model, f"/content/mlm-modeing/{model_name}/flax_model.msgpack")
     model.save_pretrained(f"/content/drive/MyDrive/inisw08/{args.model_name}")
 
     # tokenizer
     tokenizer = AutoTokenizer.from_pretrained(f"{model_name}")
     tokenizer.save_pretrained(f"/content/drive/MyDrive/inisw08/{args.model_name}") 
+    print('드라이이브 모델 저장 완료!!!!!')
 
   else:
     config = T5Config.from_pretrained(f"{model_name}")
     model = T5Model(config)
-    load_flax_checkpoint_in_pytorch_model(model, f"/content/{model_name}/flax_model.msgpack")
+    load_flax_checkpoint_in_pytorch_model(model, f"/content/mlm-modeing/{model_name}/flax_model.msgpack")
     model.save_pretrained(f"./inisw08/{args.model_name}")
     
     # tokenizer
     tokenizer = AutoTokenizer.from_pretrained(f"{model_name}")
     tokenizer.save_pretrained(f"./inisw08/{args.model_name}") 
+    print('로커어얼에 모델 저장 완료!!!!!')
 
 
 # ('.1tokenizer_config.json',
