a55071a198e9a10692b2c674e99fa16e8347e495
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 01:57:25 +0900

    Update get_bin.sh

--- get_bin.sh
+++ get_bin.sh
@@ -1,4 +1,6 @@
 pip install transformers
 git clone https://huggingface.co/ugiugi/inisw08-T5-mlm-adafactor_test
-python tool/main.py --drive --model_name inisw08-T5-mlm-adafactor_test
+python tool/main.py \
+  --drive \
+  --model_name inisw08-T5-mlm-adafactor_test
 
