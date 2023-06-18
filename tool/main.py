d504043535c6108bc917ee8be04924e383239887
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 01:56:37 +0900

    Update main.py

--- tool/main.py
+++ tool/main.py
@@ -7,7 +7,7 @@ from get_pytorch_bin import get_pytorch_bin
 if __name__ == '__main__':
 
   p = argparse.ArgumentParser()
-  p.add_argument("--drive", type=bool, default=False, help='drive 파일을 저장하고 싶다면!')
+  p.add_argument("--drive", action='store_true', help='drive 파일을 저장하고 싶다면!')
   p.add_argument("--model_name", type=str, default='inisw08-T5-mlm-adafactor_test', help='wandb, run_name')
 
   
