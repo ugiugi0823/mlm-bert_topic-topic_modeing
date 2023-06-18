b6d76ef1f43bbde13e7d2a289cbffcf6f8354029
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 02:08:43 +0900

    Update utils.py

--- tool/utils.py
+++ tool/utils.py
@@ -1,6 +1,6 @@
 import os
 def setup(args):
-  assert 'drive' in os.listdir('/content') # 당황하지 마세요! 드라이브 연결을 안해놓았어요! 코랩 드라이브 연결해주세요!
+  assert not 'drive' in os.listdir('/content') # 당황하지 마세요! 드라이브 연결을 안해놓았어요! 코랩 드라이브 연결해주세요!
   if args.drive:
     print('drive 에 모델 파일 저장됩니다.')
     os.makedirs('/content/drive/MyDrive/inisw08', exist_ok=True)
